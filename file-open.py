885f27e33f7e62a8b7b271771dc322b1e37e0c05
johnwashburne <johnwashburne@outlook.com>
2021-09-29 14:58:19 -0400

    variable metadata

--- file-open.py
+++ file-open.py
@@ -3,5 +3,11 @@ import netCDF4 as nc
 path = 'sample-files/can.ex2'
 ds = nc.Dataset(path)
 
+## print dimension metadata
 for dim in ds.dimensions.values():
     print(dim)
+
+## print variable metadata
+print("\n")
+for var in ds.variables.values():
+    print(var)
