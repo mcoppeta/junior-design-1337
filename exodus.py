7f399f1935435c2e48c12e54bc9abef11cfa93ad
johnwashburne <johnwashburne@outlook.com>
2021-10-17 15:46:24 -0400

    line printer

--- exodus.py
+++ exodus.py
@@ -9,7 +9,17 @@ class Exodus:
         for dim in self.data.dimensions.values():
             print(dim)
 
+    # prints legacy character array as string
+    def print_line(self, line):
+        s = ""
+        for c in line:
+            if str(c) != '--':
+                s += str(c)[2]
+        
+        print(s)
+        return s
 
 if __name__ == "__main__":
     ex = Exodus('sample-files/disk_out_ref.ex2')
-    print(ex.data)
+    print(ex.data.variables.keys())
+    print(ex.data['eb_status'])
