e89ef496dcee5b096f497ae554420a69f9f6dbc3
unknown <cmturko@gmail.com>
2021-10-17 17:17:05 -0400

    Added sideset parsing

--- exodus.py
+++ exodus.py
@@ -1,9 +1,27 @@
 import netCDF4 as nc
+import numpy as np
 
 class Exodus:
 
     def __init__(self, path):
         self.data = nc.Dataset(path)
+        self.nodal_coord_arr = self.data['coord']
+
+        # build sideset array
+        num_sidesets = self.data.dimensions['num_side_sets'].size
+        sidesets = []
+        for i in range(1,  num_sidesets + 1): # +1 since side sets index from 1
+            elem_key = 'elem_ss' + str(i)
+            side_key = 'side_ss' + str(i)
+            sideset_i = {}
+            sideset_i['elements'] = self.data[elem_key]
+            sideset_i['sides'] = self.data[side_key]
+            sidesets.append(sideset_i)
+
+
+            
+
+        
 
     def print_dimensions(self):
         for dim in self.data.dimensions.values():
@@ -21,5 +39,4 @@ class Exodus:
 
 if __name__ == "__main__":
     ex = Exodus('sample-files/disk_out_ref.ex2')
-    print(ex.data.variables.keys())
-    print(ex.data['eb_status'])
+    print(ex.data)
