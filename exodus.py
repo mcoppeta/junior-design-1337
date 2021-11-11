62b05e266b68ea6e60c7d6a80c95f8f95227c12a
Allen <allen.santmier@gmail.com>
2021-11-10 20:20:37 -0500

    Polish attribute part of file opening

--- exodus.py
+++ exodus.py
@@ -1,21 +1,21 @@
+import warnings
 import netCDF4 as nc
 import numpy
 from iterate import SampleFiles
 
 
 class Exodus:
-
-    _FORMAT_MAP = {'NETCDF4': 'NETCDF4',
-                    'NETCDF4_CLASSIC': 'NETCDF4_CLASSIC',
-                    'NETCDF3_LARGE_MODEL': 'NETCDF3_64BIT_OFFSET',
-                    'NETCDF3_CLASSIC': 'NETCDF3_CLASSIC'}
-    # In files, 1 is added to these numbers to allow for C compatibility
-    # Use self._max_str_length and self._max_line_length when
-    # modifying the file in case the file has special values
+    _FORMAT_MAP = {'EX_NETCDF4': 'NETCDF4',
+                   'EX_LARGE_MODEL': 'NETCDF3_64BIT_OFFSET',
+                   'EX_NORMAL_MODEL': 'NETCDF3_CLASSIC',
+                   'EX_64BIT_DATA': 'NETCDF3_64BIT_DATA'}
+    # Default values
     _MAX_STR_LENGTH = 32
+    _MAX_NAME_LENGTH = 32
     _MAX_LINE_LENGTH = 80
+    _EXODUS_VERSION = 7.22
 
-    def __init__(self, path, mode, clobber=False, format='NETCDF4', word_size=4):
+    def __init__(self, path, mode, clobber=False, format='EX_NETCDF4', word_size=4):
         # clobber and format and word_size only apply to mode w
         if mode not in ['r', 'w', 'a']:
             raise ValueError("mode must be 'w', 'r', or 'a', got '{}'".format(mode))
@@ -33,31 +33,58 @@ class Exodus:
         except OSError:
             raise OSError("file '{}' exists, but clobber is set to False".format(path))
 
-        # init self._float
+        if mode == 'w' or mode == 'a':
+            # This is important according to ex_open.c
+            self.data.set_fill_off()
+
+        # We will read a bunch of data here to make sure it exists and warn the user if they might want to fix their
+        # file. We don't save anything to memory so that if our data updates we don't have to update it in memory too.
+        # This is the same practice used in the C library so its probably a good idea.
+
+        # Initialize all the important parameters
         if mode == 'w':
-            # Spaces seem to be preferred over underscores
-            self.data.setncattr('floating point word size', word_size)
+            self.data.createDimension('title', 'Untitled database')
+            self.data.createDimension('len_string', Exodus._MAX_STR_LENGTH + 1)
+            self.data.createDimension('len_name', Exodus._MAX_NAME_LENGTH + 1)
+            self.data.createDimension('len_line', Exodus._MAX_LINE_LENGTH + 1)
+            self.data.setncattr('maximum_name_length', Exodus._MAX_NAME_LENGTH)
+            self.data.setncattr('version', Exodus._EXODUS_VERSION)
+            self.data.setncattr('api_version', Exodus._EXODUS_VERSION)
+            self.data.setncattr('floating_point_word_size', word_size)
+            file_size = 0
+            if nc_format == 'NETCDF3_64BIT_OFFSET':
+                file_size = 1
+            self.data.setncattr('file_size', file_size)
+            int64bit_status = 0
+            if nc_format == 'NETCDF3_64BIT_DATA':
+                int64bit_status = 1
+            self.data.setncattr('int64_status', int64bit_status)
+
+        # Warn the user if some non-critical parameters are missing
+        if 'len_string' not in self.data.dimensions:
+            warnings.warn("'len_string' dimension is missing!")
+        if 'len_name' not in self.data.dimensions:
+            warnings.warn("'len_name' dimension is missing!")
+        if 'len_line' in self.data.dimensions:
+            warnings.warn("'len_line' dimension is missing!")
+        if 'maximum_name_length' in self.data.ncattrs():
+            warnings.warn("'maximum_name_length' attribute is missing!")
+
+        # Check version compatibility
+        ver = self.version
+        if ver < 2.0:
+            raise RuntimeError(
+                "Unsupported file version {:.2f}! Only versions >2.0 are supported.".format(ver))
+
+        # Read word size stored in file
         ws = self.word_size
         if ws == 4:
+            # This needs to be double checked because our examples use float32 and float64 which may be different
             self._float = numpy.single
         elif ws == 8:
             self._float = numpy.double
         else:
-            raise ValueError("word size of {} not supported".format(ws))
-
-        # init self._max_str/line_length
-        if mode == 'w':
-            # This is probably required to make this backwards compatible with C
-            self.data.createDimension('len_string', Exodus._MAX_STR_LENGTH + 1)
-            self.data.createDimension('len_line', Exodus._MAX_LINE_LENGTH + 1)
-        if 'len_string' in self.data.dimensions and 'len_line' in self.data.dimensions:
-            # This is probably pointless, but safe
-            self._max_str_length = self.data.dimensions['len_string'].size - 1
-            self._max_line_length = self.data.dimensions['len_line'].size - 1
-        else:
-            self._max_str_length = Exodus._MAX_STR_LENGTH
-            self._max_line_length = Exodus._MAX_LINE_LENGTH
-
+            raise ValueError("file contains a word size of {} which is not supported".format(ws))
 
     def to_float(self, n):
         # Convert a number to the floating point type the file is using
@@ -67,13 +94,9 @@ class Exodus:
     def float(self):
         return self._float
 
-    @property
-    def max_str_length(self):
-        return self._max_str_length
-
-    @property
-    def max_line_length(self):
-        return self._max_line_length
+    # TODO fix function that adds missing parts of the header
+    # TODO function to find the longest name in the object
+    # TODO maximum_name_length should be the actual length, not with the 1 C character added. confirm this
 
     ########################################################################
     #                                                                      #
@@ -90,34 +113,80 @@ class Exodus:
         return result
 
     @property
-    def word_size(self):
-        # Both styles of spacing are used in Exodus
-        params = self.parameters
-        if 'floating_point_word_size' in params:
-            return self.data.getncattr('floating_point_word_size')
-        elif 'floating point word size' in params:
-            return self.data.getncattr('floating point word size')
-        else:
-            raise RuntimeError("parameter 'floating point word size' cannot be found!")
+    def title(self):
+        # Guaranteed to exist
+        return self.data.getncattr('title')
+
+    @property
+    def max_string_length(self):
+        max_str_len = Exodus._MAX_STR_LENGTH
+        if 'len_string' in self.data.dimensions:
+            # Subtract 1 because in C an extra character is added for C reasons
+            max_str_len = self.data.dimensions['len_string'].size - 1
+        return max_str_len
+
+    @property
+    def max_line_length(self):
+        max_line_len = Exodus._MAX_LINE_LENGTH
+        if 'len_line' in self.data.dimensions:
+            # Subtract 1 because in C an extra character is added for C reasons
+            max_line_len = self.data.dimensions['len_line'].size - 1
+        return max_line_len
+
+    @property
+    def max_allowed_name_length(self):
+        max_name_len = Exodus._MAX_NAME_LENGTH
+        if 'len_name' in self.data.dimensions:
+            # Subtract 1 because in C an extra character is added for C reasons
+            max_name_len = self.data.dimensions['len_name'].size - 1
+        return max_name_len
+
+    @property
+    def max_used_name_length(self):
+        max_used_name_len = 0
+        if 'maximum_name_length' in self.data.ncattrs():
+            max_used_name_len = self.data.getncattr('maximum_name_length')
+        return max_used_name_len
 
     @property
     def api_version(self):
-        # Both styles of spacing are used in Exodus
+        # Guaranteed to exist
         params = self.parameters
         if 'api_version' in params:
             return self.data.getncattr('api_version')
         elif 'api version' in params:
             return self.data.getncattr('api version')
-        else:
-            raise RuntimeError("parameter 'api version' cannot be found!")
 
     @property
     def version(self):
+        # Guaranteed to exist
         return self.data.getncattr('version')
 
     @property
-    def title(self):
-        return self.data.getncattr('title')
+    def file_size(self):
+        if 'file_size' in self.data.ncattrs():
+            return self.data.getncattr('file_size')
+        else:
+            return 1 if self.data.data_model == 'NETCDF3_64BIT_OFFSET' else 0
+            # No warning is raised because older files just don't have this
+
+    @property
+    def int64_status(self):
+        # This might actually be for NETCDF5 files but I'm not certain
+        if 'int64_status' in self.data.ncattrs():
+            return self.data.getncattr('int64_status')
+        else:
+            return 1 if self.data.data_model == 'NETCDF3_64BIT_DATA' else 0
+            # No warning is raised because older files just don't have this
+
+    @property
+    def word_size(self):
+        # Guaranteed to exist
+        params = self.parameters
+        if 'floating_point_word_size' in params:
+            return self.data.getncattr('floating_point_word_size')
+        elif 'floating point word size' in params:
+            return self.data.getncattr('floating point word size')
 
     @property
     def qa_records(self):
@@ -179,7 +248,7 @@ class Exodus:
     def time_steps(self):
         """Returns list of the time steps, 0-indexed"""
         return [*range(self.num_time_steps)]
-    
+
     def timeAtStep(self, step):
         """Given an integer time step, return the corresponding float time value"""
         return float(self.data['time_whole'][step].data)
@@ -232,7 +301,7 @@ class Exodus:
             sideset_i['sides'] = self.data[side_key]
         else:
             raise RuntimeError("sideset '{}' cannot be found!".format(i))
-            
+
         return sideset_i
 
     def get_nodeset(self, i):
@@ -255,12 +324,12 @@ class Exodus:
 
 
 if __name__ == "__main__":
-    for file in SampleFiles():
-        ex = Exodus(file, 'r')
-        try:
-            print(ex.qa_records)
-        except KeyError:
-            print("no QA record found")
-
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        for file in SampleFiles():
+            ex = Exodus(file, 'r')
+            try:
+                print(ex.data)
+            except KeyError:
+                print("no QA record found")
         ex.close()
-
