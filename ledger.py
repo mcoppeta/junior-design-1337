8f81ffd717c799e8c0b4e8354f3ac3971a68d97c
unknown <cmturko@gmail.com>
2022-01-23 15:18:08 -0500

    Added maps for dimensions and vars in ledger

--- ledger.py
+++ ledger.py
@@ -1,7 +1,21 @@
 class Ledger:
     
     def __init__(self):
-        size = 0
+        self.size = 0
+        self.dimension_map = {}
+        self.variable_map = {}
+
+
+    def addDimension(self, dim):
+        self.dimension_map[dim.name] = dim
+
+
+
+    def addVariable(self, var):
+        self.variable_map[var.name] = var
+
+    
+
 
 
 
