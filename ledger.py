f170a8f6774c1ca7cf836051beecf7da53b125d7
John Washburne Jr <johnwashburne@outlook.com>
2022-03-07 12:45:40 -0500

    add docstrings to ledger.py

--- ledger.py
+++ ledger.py
@@ -12,36 +12,95 @@ class Ledger:
 
 
     def num_node_sets(self):
+        """ Returns the number of nodesets present in the file"""
         return self.nodeset_ledger.num_node_sets()
 
     def get_node_set(self, id):
+        """
+
+        :param id: the id of the requested node set
+        :return: ndarray of all of the node ids that belong to the specified node set
+        """
         return self.nodeset_ledger.get_node_set(id)
 
     def get_node_set_name(self, id):
+        """
+
+        :param id: the id of the requested node set
+        :return: the name of the specified node set
+        """
         return self.nodeset_ledger.get_node_set_name(id)
 
     def get_node_set_names(self):
+        """
+        Returns array of nodeset names
+        :return: array of nodeset names
+        """
         return self.nodeset_ledger.get_node_set_names()
 
     def add_nodeset(self, node_ids, nodeset_id, nodeset_name=""):
+        """
+
+        :param node_ids: the node ids which will be contained within the new nodeset
+        :param nodeset_id: the intended id for the new nodeset
+        :param nodeset_name: the intended name for the new nodeset
+        :return: None
+        """
         self.nodeset_ledger.add_nodeset(node_ids, nodeset_id, nodeset_name)
 
     def remove_nodeset(self, nodeset_id):
+        """
+        Removes given nodeset from the Exodus file
+        :param nodeset_id: the id of the nodeset being removed
+        :return: None
+        """
         self.nodeset_ledger.remove_nodeset(nodeset_id)
 
-    def merge_nodesets(self, new_id, ns1, ns2, delete):
+    def merge_nodesets(self, new_id, ns1, ns2, delete=True):
+        """
+        Merges 2 nodesets together
+        :param new_id: the intended ID for the new nodeset
+        :param ns1: the first nodeset to merge
+        :param ns2: the second nodeset to merge
+        :param delete: a flag which specifies whether the 2 starting nodesets will be removed
+        :return: None
+        """
         self.nodeset_ledger.merge_nodesets(new_id, ns1, ns2, delete)
 
     def add_node_to_nodeset(self, node_id, nodeset_id):
+        """
+        Add one node to the specified nodeset
+        :param node_id: the node being added to the nodeset
+        :param nodeset_id: the nodeset the node will be added to
+        :return: None
+        """
         self.nodeset_ledger.add_node_to_nodeset(node_id, nodeset_id)
 
     def add_nodes_to_nodeset(self, node_ids, nodeset_id):
+        """
+        Add many nodes to the specified nodeset
+        :param node_ids: the nodes being added to the nodeset (List-like)
+        :param nodeset_id: the nodeset being added to
+        :return: None
+        """
         self.nodeset_ledger.add_nodes_to_nodeset(node_ids, nodeset_id)
 
     def remove_node_from_nodeset(self, node_id, nodeset_id):
+        """
+        Remove single node from a specified nodeset
+        :param node_id: the node being removed from the nodeset
+        :param nodeset_id: the nodeset being removed from
+        :return: None
+        """
         self.nodeset_ledger.remove_node_from_nodeset(node_id, nodeset_id)
 
     def remove_nodes_from_nodeset(self, node_ids, nodeset_id):
+        """
+        Remove many nodes from a specified nodeset
+        :param node_ids: the nodes being removed from the nodeset (List-like)
+        :param nodeset_id: the nodeset being removed from
+        :return: None
+        """
         self.nodeset_ledger.remove_nodes_from_nodeset(node_ids, nodeset_id)
 
     # sideset methods
@@ -53,6 +112,10 @@ class Ledger:
 
 
     def write(self):
+        """
+        Write from the ledger
+        :return: None
+        """
         if self.ex.mode == 'w':
             self.w_write()
         elif self.ex.mode == 'a':
