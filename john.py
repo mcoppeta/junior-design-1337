3a75ba098c40efd92d304b0a4d53219d96ca757c
John Washburne Jr <johnwashburne@outlook.com>
2022-04-03 14:20:32 -0400

    cleanup files

--- john.py
+++
@@ -1,9 +0,0 @@
-from exodus import Exodus
-from iterate import SampleFiles
-import netCDF4 as nc
-
-data = nc.Dataset('sample-files/w/test.ex2', 'w', format='NETCDF3_CLASSIC')
-
-data.createDimension('dim1', 0)
-data.createDimension('dim2', 0)
-data.createDimension('dim3', 0)
