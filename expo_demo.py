7dce2830c7f9b735f8f0c482b7610860998d2a1e
Michael Coppeta <mcoppeta@gmail.com>
2022-04-24 19:46:58 -0400

    Change subset to tail and skin it

--- expo_demo.py
+++ expo_demo.py
@@ -4,6 +4,11 @@ if __name__ == "__main__":
     ex = Exodus("sample-files/biplane.exo", 'r')
     # Output the propeller to a new file
     # EB: 34, 35, 36, 37, 38
-    eb_sels = [ElementBlockSelector(ex, 34), ElementBlockSelector(ex, 35), ElementBlockSelector(ex, 36),
-               ElementBlockSelector(ex, 37), ElementBlockSelector(ex, 38)]
-    output_subset(ex, "sample-files/propeller.exo", "Biplane Propeller", eb_sels, [], [], PropertySelector(ex))
+    eb_sels = [ElementBlockSelector(ex, 1), ElementBlockSelector(ex, 3), ElementBlockSelector(ex, 4)]
+    output_subset(ex, "sample-files/w/tail.exo", "Biplane Propeller", eb_sels, [], [], PropertySelector(ex))
+    ex.close()
+
+    ex = Exodus("sample-files/w/tail.exo", 'a')
+    ex.skin(3312, 'Skinned Mesh')
+    ex.write("sample-files/w/tail-skinned.exo")
+    ex.close()
