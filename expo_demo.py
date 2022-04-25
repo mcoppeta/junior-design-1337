294ffc1dc7cef9e080b810dc73ebb99223ceee47
Michael Coppeta <mcoppeta@gmail.com>
2022-04-24 20:07:39 -0400

    did something'

--- expo_demo.py
+++ expo_demo.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
 
     ex = Exodus("sample-files/w/tail.exo", 'a')
     ex.skin(3312, 'Skinned Mesh')
+    ex.split_side_set_x_coords(3312, '>=', 0.849, True, 1111, 2222, False, ss_name1='>=0.849', ss_name2='<0.849')
     ex.write("sample-files/w/tail-skinned.exo")
     ex.close()
