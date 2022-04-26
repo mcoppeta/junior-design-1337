87cf3067250e4ef43ea92342c03cfcd2277b7b0c
Allen <allen.santmier@gmail.com>
2022-04-25 22:00:55 -0400

    Fix unused functions and docstrings

--- exodusutils/__init__.py
+++ exodusutils/__init__.py
@@ -17,8 +17,9 @@ assembly.
 
 # Tutorial
 ## Opening and closing an Exodus II file
-To open an Exodus II file, create an instance of the `exodusutils.exodus.Exodus` class with the path to the file you want to open and the
-mode you want to open it in. Mode 'r' opens the file in read only mode and mode 'a' opens it for reading and writing.
+To open an Exodus II file, create an instance of the `exodusutils.exodus.Exodus` class with the path to the file you
+want to open and the mode you want to open it in. Mode 'r' opens the file in read only mode and mode 'a' opens it for
+reading and writing.
 
 When you are done working with the Exodus II file, call the `close` function on the `Exodus` object. This both closes
 the object itself, meaning you cannot use it anymore, and closes access to the actual file on disk as well.
@@ -37,10 +38,86 @@ ex.close()
 ```
 ## Difference between two Exodus II files
 ## Output a subset of an Exodus II file
-+
+If you have a big Exodus file and want to work with only a part of the whole mesh, you can use
+`exodusutils.output_subset` to output a part of the mesh to a new file.
+
+`output_subset` has many options for selecting which part of the mesh you want to write to a new file. To assist with
+selecting parts of each individual block and set, `output_subset` takes in lists of selectors from
+`exodusutils.selector`. Each selector corresponds to a single block or set from the input file and allows you to choose
+which elements/sides/nodes you want to keep, which variables you want to keep, and which attributes to keep (for element
+blocks). `output_subset` also requires one `exodusutils.selector.PropertySelector` to choose which element block,
+node set, and side set properties you want to keep. Do note that if you do not select the 'ID' property all user-defined
+block and set IDs will be lost in the output file. After the selectors you can also pass in a list of nodal and global
+variable IDs to keep and a list of time steps to keep.
+
+Below is a step-by-step process of writing out a subset of a mesh.
+1. Create an empty list to contain your `ElementBlockSelectors`.
+2. For each element block you want to keep, append an `exodusutils.selector.ElementBlockSelector` for that element
+block, selecting the elements you want to keep and any variables or attributes you want to keep.
+3. Create an empty list to contain your `SideSetSelectors`.
+4. For each side set you want to keep, append a `exodusutils.selector.SideSetSelector` for that side set, selecting the
+sides you want to keep as well as any variables you want to keep. *Any element contained in the selected part of the
+side set must also be selected in your list of ElementBlockSelectors!*
+5. Create an empty list to contain your `NodeSetSelectors`.
+6. As before, append a `exodusutils.selector.NodeSetSelector` for each node set containing the nodes and variables you
+want to keep.
+7. Create a `exodusutils.selector.PropertySelector` and pass in the lists of properties you want to keep per its
+constructor.
+8. Make a list of all of the nodal and global variable IDs you want to keep.
+9. Make a list of all of the time steps you want to keep.
+10. Pass all of the lists along with other required arguments to `exodusutils.output_subset.output_subset`.
+
+By default, if you do not pass in an argument to a selector, it selects all applicable items. If you want to quickly do
+this explicitly you should pass in `...` and if you want to quickly select nothing you should pass in either an empty
+list or `None`. The same applied for the nodal variable ID list, global variable ID list, and time steps list.
+
+If an error occurs during execution of the `output_subset` function, the output file it creates will be corrupt!
+
+```
+# We are going to output only the tail of the biplane.
+ex = Exodus("sample-files/biplane.exo", 'r')
+# Propeller element blocks: 1, 3, 4
+eb_sels = [ElementBlockSelector(ex, 1), ElementBlockSelector(ex, 3), ElementBlockSelector(ex, 4)]
+# We don't want to keep any node sets or side sets, so leave those lists empty.
+# We don't want to lose any properties so we leave the property lists in the
+# PropertySelector constructor to their default value (all).
+# We want to keep all the variables and time steps, so we leave those
+# arguments to their default values
+output_subset(ex, "sample-files/w/tail.exo", "Biplane Tail", eb_sels, [], [], PropertySelector(ex))
+ex.close()
+```
 ## Skinning a mesh
-## Adding data to an Exodus II file
-## Removing data from an Exodus II file
+## Adding/Removing/Modifying data in an Exodus II file
+To alter an Exodus II file, create the `Exodus` object in append mode (mode='a'). In read mode, calling any functions
+that modify the file will cause an error to be thrown. Any modifications you make to the file will only be stored in
+memory. When you are done modifying the file you should call the `Exodus` object's `write` function with a path to the
+file you would like to output the changes to. You must pass in a new path so that you do not overwrite the source file
+and risk losing important data.
+
+All of the functions in `exodusutils.exodus.Exodus` below the diff functions modify the file and require append mode.
+
+This library does not currently support modifying variables, but you can add and remove nodes/sides from node/side sets
+as well as create brand new node/side sets and delete existing ones in addition to adding and removing elements. This
+library also supplies helpful functions for splitting side sets and skinning element blocks, both of which result in the
+creation of new side sets.
+
+```
+# Open the Exodus II file in append mode
+ex = Exodus("sample-files/w/tail.exo", 'a')
+# Skin the whole mesh and output the side set containing all exterior faces to
+# a new side set with ID 3312
+ex.skin(3312, 'Skinned Mesh')
+# Split the newly created side set 3312 into side set 1111 containing only
+# faces entirely to the right of x=0.849 and 222 containing those to the
+# left of x=0.849 without deleting side set 3312
+ex.split_side_set_x_coords(3312, '>=', 0.849, True, 1111, 2222, False)
+# Now that we have made changes to the file, write those changes to a new
+# file called "tail-skinned.exo"
+ex.write("sample-files/w/tail-skinned.exo")
+# We've written our changes so now we can safely close the original file
+# without losing our progress
+ex.close()
+```
 """
 
 from .exodus import Exodus
